58608db7f108934ddb624e88c02ddc20c365999a
Nana9989 <nanaarhin57@gmail.com>
2024-06-30 23:31:10 +0000

    timestable

---
+++ control-flow/test.py
@@ -0,0 +1,6 @@
+number = int(input("enter number"))
+for i in range(1,11):
+  for j in range(1, 11):
+    # Inner loop iterates through columns (other factors)
+    product = i * j
+    print(f"{i} x {j} = {product}", end="\t")  # Print with tabs for better formatting
