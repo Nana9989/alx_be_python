a24a03e4e007afddaad759f9f730e06ce0ce4d9b
Nana9989 <nanaarhin57@gmail.com>
2024-07-03 15:12:22 +0000

    pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,18 +1,6 @@
-i = 1
-
-while i <= 5:
-	print("*", end="")
-	j=1
-	while j >= 1:
+pattern = int(input("Enter the size of the pattern:"))
+for i in range(pattern):
+	for j in range(pattern):
 		print("*", end="")
-		j=j-1
-		k=1
-		while k >=1:
-			print("*", end="")
-			k=k-1
-			l=1
-			while l >=1:
-					print("*", end="")
-					l=l-1
-	i=i+1
-	print( )
+
+	print()
