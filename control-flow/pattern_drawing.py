34e05ec34b973f90e1e198cb9898fee333a74048
Nana9989 <nanaarhin57@gmail.com>
2024-07-03 13:50:09 +0000

    pattern1

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,18 @@
+i = 1
+
+while i <= 5:
+	print("*", end="")
+	j=1
+	while j >= 1:
+		print("*", end="")
+		j=j-1
+		k=1
+		while k >=1:
+			print("*", end="")
+			k=k-1
+			l=1
+			while l >=1:
+					print("*", end="")
+					l=l-1
+	i=i+1
+	print( )
