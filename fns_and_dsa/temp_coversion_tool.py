5967277d2ed21b365d0132d7353ca5431c9e9931
Nana9989 <nanaarhin57@gmail.com>
2024-07-07 23:44:47 +0000

    shopping list2

---
+++ fns_and_dsa/temp_coversion_tool.py
@@ -0,0 +1,14 @@
+FARENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FARENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(farenheit):
+    answer = (farenheit - 32)*FARENHEIT_TO_CELSIUS_FACTOR
+    return answer
+
+def convert_to_farenheit(celsius):
+    answer = (celsius * CELSIUS_TO_FARENHEIT_FACTOR) + 32
+    return answer
+
+temperature = int(input("Enter the temperature to convert: "))
+unit = input("Is this temperature in Celsius or Farenheit? (C/F): ")
+
