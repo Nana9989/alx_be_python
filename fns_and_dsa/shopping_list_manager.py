3549d829a5a0c449e1cb7122357b03e6ba20e307
Nana9989 <nanaarhin57@gmail.com>
2024-07-07 23:29:00 +0000

    shopping list2

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,7 +10,7 @@ def main():
     while True:
         choice = input("Enter your choice: ")
         if choice == "1":
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} has been added")
         elif choice == "2":
