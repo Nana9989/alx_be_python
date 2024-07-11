4975c59e9d3c1d00ae02c94357a3a15bd15971f7
Nana9989 <nanaarhin57@gmail.com>
2024-07-11 13:02:16 +0000

    temp4

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,11 +5,35 @@ def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+
+def main():
+    try:
+        temperature = float(input("Enter temperature to convert: "))
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+
+        if unit == "C":
+            converted_temperature = convert_to_fahrenheit(temperature)
+            print(f"{temperature} °C is {converted_temperature} °F")
+
+        elif unit == "F":
+            converted_temperature = convert_to_celsius(temperature)
+            print(f"{temperature} °F is {converted_temperature} °C")
+
+        else:
+            raise ValueError("Invalid temperature format. Please specify unit as C or F")
+
+    except ValueError as e:
+        print(f"Error: {e}")
+        print("Invalid temperature. Please enter a numeric value followed by '°F' or '°C'.")
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
 
 
 
