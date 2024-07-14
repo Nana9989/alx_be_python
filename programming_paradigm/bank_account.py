f830cb10388110e8748541aa859028e69a18c2be
Nana9989 <nanaarhin57@gmail.com>
2024-07-14 22:54:39 +0000

    test

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance +=amount
+        return self.account_balance
+    def withdrawl(self, amount):
+        if self.account_balance >= amount:
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"The current balance is: $")
+
+
