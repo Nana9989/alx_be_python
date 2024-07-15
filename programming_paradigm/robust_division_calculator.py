d054b8c46ea7f043f6f02927d65419866a360517
Nana9989 <nanaarhin57@gmail.com>
2024-07-15 00:01:39 +0000

    tst2

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,9 @@
 def safe_divide(numerator, denominator):
     return numerator/denominator
 
+numerator = input(float("numerator"))
+denominator = input(float("denominator"))
+
 try:
     safe_divide()
 except ZeroDivisionError :
