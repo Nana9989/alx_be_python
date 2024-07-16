4a3d9fd67159b8a698ee27324e9a64dc6dde4a3e
Nana9989 <nanaarhin57@gmail.com>
2024-07-16 15:57:44 +0000

    test1

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+def safe_divide(numerator, denominator):
+    try:
+        a = float(numerator)
+        b = float(denominator)
+
+        result = a / b
+
+    except ZeroDivisionError:
+        return "Eror: Cannot divide by zero."
+
+    except ValueError:
+        return "Error: Please enter numeric values only."
+
+    return f"The result is {result:.2f}"
